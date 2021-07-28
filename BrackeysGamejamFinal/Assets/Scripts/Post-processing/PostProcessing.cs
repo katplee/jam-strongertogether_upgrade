fd72c014b3a01aa26ea9bdb92db694b6e3743fab
katplee <62573178+katplee@users.noreply.github.com>
2021-07-28 20:50:02 +0900

    Post-upgrade to URP github push
    Converted the camera. Added a child to the camera game object as an example for doing shaders, etc. in the future. Added a script inside a new folder named "Post-processing" as an example.

---
+++ BrackeysGamejamFinal/Assets/Scripts/Post-processing/PostProcessing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+//UnityEngine.Rendering gives access to the post-prcessing profile
+
+public class PostProcessing : MonoBehaviour
+{
+    private Volume volume;
+    private Bloom bloom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = GetComponent<Volume>();
+        volume.profile.TryGet(out bloom);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
